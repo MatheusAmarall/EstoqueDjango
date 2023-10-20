47e8690f770c033c1241a1600d3d84f75528fe9c
MATHEUS DA SILVA AMARAL <matheus.amaral@alunos.sc.senac.br>
2023-10-19 21:41:46 -0300

    criando tela product_detail, adicionando novas tabelas, adicionando imagens

--- conf/settings.py
+++ conf/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
