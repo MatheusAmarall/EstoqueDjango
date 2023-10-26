1ad87f2161a1de3559068aa83e7dcb52163f11e2
MATHEUS DA SILVA AMARAL <matheus.amaral@alunos.sc.senac.br>
2023-10-25 21:28:43 -0300

    adicionando e removendo produto

--- stockdjango/urls.py
+++ stockdjango/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('product-detail/<int:id>', views.product_detail, name='product-detail')
+    path('add-product/', views.add_product, name='add-product'),
+    path('product-detail/<int:id>', views.product_detail, name='product-detail'),
+    path('delete-product/<int:id>', views.delete_product, name='delete-product')
 ]
