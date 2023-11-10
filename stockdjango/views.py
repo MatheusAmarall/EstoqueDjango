ef3c75e0d422ed2edcdfa3964b48abdee13af913
MATHEUS DA SILVA AMARAL <matheus.amaral@alunos.sc.senac.br>
2023-11-09 21:11:43 -0300

    criacao de usuario e login required no index

--- stockdjango/views.py
+++ stockdjango/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Products, Categories
 from random import randint
-from datetime import datetime 
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
-def index(request):    
+@login_required(redirect_field_name='login')
+def index(request):
     produtos = Products.objects.filter(in_stock=True)
     return render(request, 'pages/index.html', {'produtos':produtos})
 
